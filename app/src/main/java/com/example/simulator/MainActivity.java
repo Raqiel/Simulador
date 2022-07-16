d20311ae808b3ec96b45ceb0a913ee2db69c2d8b
raque <raquel.oliveira.goncalves@outlook.com>
2022-07-16 14:12:30 -0300

    Changes

---
+++ app/src/main/java/com/example/simulator/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.simulator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.simulator.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding= ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
